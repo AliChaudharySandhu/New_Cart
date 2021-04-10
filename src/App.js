83291d5136460cd197aee61a6f93c7019f9ccc0a
AliChaudharySandhu <AliChaudhary100@gmail.com>
2021-04-10 15:33:19 +0500

    my second commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid__container">
+        <header>
+          <a>Shoping Cart</a>
+        </header>
+
+        <section className="main">
+          <p>Product List</p>
+
+        </section>
+        
+        <footer>
+          <p>All rights reserved!</p>
+        </footer>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// Adding New Feature
